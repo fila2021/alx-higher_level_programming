b464f53b63f40e4fa9eb2e3985186ee395924e0a
Filmawit Mekonen <zanta.nahu1411@gmail.com>
2022-07-06 01:22:26 +0400

    fix

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+''' function that returns an object (Python data structure) represented by a JSON string
+'''
+
 import json
 
 
 def from_json_string(my_str):
-    """Python object representation"""
+    ''' module from_json_string
+     returns Python objects
+    '''
     return json.loads(my_str)
